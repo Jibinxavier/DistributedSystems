a5c29eaff3977a71207f42b9e79f4bfe47dd9938
Jibinxavier <xavierj@tcd.ie>
2019-02-25 19:18:01 +0000

    added components header and files

--- authserverui/src/index.js
+++ authserverui/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import App from './components/App';
+import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import { createStore} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const defaultState = {
+  appName: 'Distributed Filesystem',
+  files: null
+};
+const reducer = function(state = defaultState, action){
+    return state;
+  };
+  const store = createStore(reducer);
+  
+  
+  ReactDOM.render((
+    <Provider store={store}>
+    <App/>
+    </Provider>
+    
+  ), document.getElementById('root'));
